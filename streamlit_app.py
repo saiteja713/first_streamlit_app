4acdc35adb559764c02aa01ea793fb9c1b4a276c
saiteja713 <107686708+saiteja713@users.noreply.github.com>
2023-07-12 20:41:27 +0530

    Update streamlit_app.py
    Add close function for snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,7 @@ except URLError as e:
 def fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
+    my_cnx.close()
     return my_cur.fetchall()
 
 #Add a button to load the fruit
